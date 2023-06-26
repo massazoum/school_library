1e9fc6f42171988f603b096697bf16d7db418f84
massa <zoumaniguimassa12@gmail.com>
2023-06-26 18:43:24 +0000

    Class person student  teacheer created and readme updated

---
+++ person_class.rb
@@ -0,0 +1,27 @@
+class Person
+ attr_reader :id
+ attr_accessor :name, :age
+
+ def initialize(name = "Unknown", age = nil, parent_permission = true)
+   @id = generate_id
+   @name = name
+   @age = age
+   @parent_permission = parent_permission
+ end
+
+ def of_age?
+   return false if @age.nil?
+
+   @age >= 18
+ end
+
+ def can_use_services?
+   of_age? || @parent_permission
+ end
+
+ private
+
+ def generate_id
+   rand(1..9999)
+ end
+end

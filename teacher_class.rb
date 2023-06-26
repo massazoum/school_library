b8c5e8295c969f863b7ec2801fdb4fe7aab4d7f1
massa <zoumaniguimassa12@gmail.com>
2023-06-26 18:54:09 +0000

    Classes updated

--- teacher_class.rb
+++ teacher_class.rb
@@ -1,12 +1,12 @@
 require_relative 'person_class'
 
 class Teacher < Person
- def initialize(name = "Unknown", age = nil, parent_permission = true, specialization)
-   super(name, age, parent_permission)
-   @specialization = specialization
- end
+  def initialize(specialization, name = 'Unknown', age = nil, parent_permission: true)
+    super(name, age, parent_permission)
+    @specialization = specialization
+  end
 
- def can_use_services?
-   true
- end
+  def can_use_services?
+    true
+  end
 end
